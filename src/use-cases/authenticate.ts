9eceebe6cf79d7b3f37fe1f4fb268135ad36273c
samuel_alencar <samuel_alencar@carrefour.com>
2023-12-15 17:51:49 -0300

    fix: file name correction

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -1,5 +1,5 @@
 import { User } from '@prisma/client';
-import { UsersRepository } from './../repositories/users-repository';
+import { UsersRepository } from '../repositories/users-repository';
 import { InvalidCredentialsError } from './errors/invalid-credentials-error.ts';
 import { compare } from 'bcryptjs';
 
